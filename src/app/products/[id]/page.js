3a8abcc37af59b0d2d3dd5cb6ad4be9b3f3132ac
Ram Charan G Gaddamidhi <ramgoud696@gmail.com>
2026-03-11 09:58:49 +0530

    updated products/id pageb

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { notFound } from "next/navigation";
 import { publicApi } from "@/lib/apiClient";
 import ProductDetailsContent from "@/components/ProductDetailsContent";
