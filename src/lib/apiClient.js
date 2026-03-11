054c50aef5b3dae49d6905b621065c085cb5a9f6
Ram Charan G Gaddamidhi <ramgoud696@gmail.com>
2026-03-11 09:35:08 +0530

    corrected base url

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -21,7 +21,7 @@ export async function apiRequest(path, options = {}) {
     headers = {},
   } = options;
 
-  const baseUrl = "https://murgan-backend-1.onrender.comapi";
+  const baseUrl = "https://murgan-backend-1.onrender.com/api";
 
   if (!baseUrl) {
     throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined in .env.local");
